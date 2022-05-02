124d7f6aa2e44aa31daa547a8e2041b9742da5a8
Guilherme Marcel <guigui.marcel51@gmail.com>
2022-05-02 20:00:59 -0300

    fix: problems with module versisons

--- components/login.js
+++ components/login.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import Grid from '@material-ui/core/Grid'
-import { Button } from '@material-ui/core'
-import Paper from '@material-ui/core/Paper'
-import { TextField } from '@material-ui/core'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import { TextField } from '@mui/material'
 import { useRouter } from 'next/router'
 
 
